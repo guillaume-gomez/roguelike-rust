79cf4558e7d91627cb30058391a871e8ab98ee41
Guillaume Gomez <guillaume.gomez846@gmail.com>
2019-10-02 20:57:05 +0200

    move main character

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::input::Key;
+use tcod::input::KeyCode::*;
 
 // actual size of the window
 const SCREEN_WIDTH: i32 = 80;
@@ -9,6 +11,21 @@ const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
 struct Tcod {
     root: Root,
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    // todo: handle keys
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        // movement keys
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        _ => {}
+    }
+    false
 }
 
 fn main() {
@@ -22,10 +39,17 @@ fn main() {
     let mut tcod = Tcod { root };
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(RED);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
+        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
     }
