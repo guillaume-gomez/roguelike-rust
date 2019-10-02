b2853b8396491e98ca8b41e2fdaaa96d22132cdc
Guillaume Gomez <guillaume.gomez846@gmail.com>
2019-10-02 21:17:18 +0200

    using second window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tcod::colors::*;
+use tcod::colors;
 use tcod::console::*;
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
@@ -11,6 +11,7 @@ const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
 struct Tcod {
     root: Root,
+    con: Offscreen,
 }
 
 fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
@@ -36,20 +37,31 @@ fn main() {
     .title("Rust/libtcod tutorial")
     .init();
 
-    let mut tcod = Tcod { root };
+    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    let mut tcod = Tcod { root, con };
     tcod::system::set_fps(LIMIT_FPS);
 
     let mut player_x = SCREEN_WIDTH / 2;
     let mut player_y = SCREEN_HEIGHT / 2;
 
     while !tcod.root.window_closed() {
-        tcod.root.set_default_foreground(RED);
-        tcod.root.clear();
+        tcod.con.clear();
         let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
         if exit {
             break;
         }
-        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        tcod.con.set_default_foreground(colors::WHITE);
+        tcod.con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        blit(
+            &tcod.con,
+            (0, 0),
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            &mut tcod.root,
+            (0, 0),
+            1.0,
+            1.0,
+        );
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
     }
