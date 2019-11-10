09704cfa61df6b9d76d6e9c13dc130129fdaef31
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-11-10 09:04:20 +0100

    add enum in fighter

--- src/fighter.rs
+++ src/fighter.rs
@@ -1,3 +1,6 @@
+use crate::object::Object;
+
+
 // combat-related properties and methods (monster, player, NPC).
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Fighter {
@@ -5,4 +8,23 @@ pub struct Fighter {
   pub hp: i32,
   pub defense: i32,
   pub power: i32,
+  //pub on_death: DeathCallback,
 }
+
+
+// #[derive(Clone, Copy, Debug, PartialEq)]
+// pub enum DeathCallback {
+//   Player,
+//   Monster,
+// }
+
+// impl DeathCallback {
+//   fn callback(self, object: &mut Object) {
+//     use DeathCallback::*;
+//     let callback: fn(&mut Object) = match self {
+//         Player => player_death,
+//         Monster => monster_death,
+//     };
+//     callback(object);
+//   }
+// }
