40045a68f58d1eb3bacd4af1e4981312f7b5e328
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-11-11 22:45:27 +0100

    move constant in a file

---
+++ src/constants.rs
@@ -0,0 +1,42 @@
+use tcod::colors::Color;
+use tcod::map::{FovAlgorithm};
+
+// GUI
+pub const BAR_WIDTH: i32 = 20;
+pub const PANEL_HEIGHT: i32 = 7;
+pub const PANEL_Y: i32 = SCREEN_HEIGHT - PANEL_HEIGHT;
+
+pub const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+
+// Colors
+pub const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+pub const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50 };
+
+pub const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+pub const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
+
+
+// size of the map (duplicated from game, TODO create a constant file)
+pub const MAP_WIDTH: i32 = 80;
+pub const MAP_HEIGHT: i32 = 43;
+// actual size of the window
+pub const SCREEN_WIDTH: i32 = 80;
+pub const SCREEN_HEIGHT: i32 = 50;
+
+// Fov algorithm
+pub const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic; // default FOV algorithm
+pub const FOV_LIGHT_WALLS: bool = true; // light walls or not
+pub const TORCH_RADIUS: i32 = 10;
+
+
+// room constants
+pub const ROOM_MAX_SIZE: i32 = 10;
+pub const ROOM_MIN_SIZE: i32 = 6;
+pub const MAX_ROOMS: i32 = 30;
+pub const MAX_ROOM_MONSTERS: i32 = 3;
+
+
+// messages
+pub const MSG_X: i32 = BAR_WIDTH + 2;
+pub const MSG_WIDTH: i32 = SCREEN_WIDTH - BAR_WIDTH - 2;
+pub const MSG_HEIGHT: usize = PANEL_HEIGHT as usize - 1;
