2c0da37715b73395e396650267282336527e4462
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-11-14 22:40:25 +0100

    comment code

--- src/death_callback.rs
+++ src/death_callback.rs
@@ -1,20 +1,18 @@
-use crate::game::Game;
-use crate::object::Object;
-use crate::fighter::Fighter;
-
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum DeathCallback {
    Player,
    Monster,
 }
 
-impl DeathCallback {
-   pub fn callback(self, object: &mut Object, game: &mut Game) {
-     use DeathCallback::*;
-     let callback: fn(&mut Object, &mut Game) = match self {
-         Player => Fighter::player_death,
-         Monster => Fighter::monster_death,
-     };
-     callback(object, game);
-  }
-}
+
+// deprecrated after splitting Enemy and Player in disctinct Struct
+// impl DeathCallback {
+//    pub fn callback(self, object: &mut Object, game: &mut Game) {
+//      use DeathCallback::*;
+//      let callback: fn(&mut Object, &mut Game) = match self {
+//          Player => Fighter::player_death,
+//          Monster => Fighter::monster_death,
+//      };
+//      callback(object, game);
+//   }
+// }
