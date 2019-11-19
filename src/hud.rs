cc76bda86fc7c6abf771d6ade9eea7cd0eb4db37
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-11-20 00:36:31 +0100

    update hud to render item option

--- src/hud.rs
+++ src/hud.rs
@@ -1,3 +1,4 @@
+use crate::object::Object;
 use crate::constants::*;
 use crate::game::Game;
 use crate::player::Player;
@@ -115,7 +116,81 @@ fn render_raycast(tcod: &mut Tcod, enemys: &[Enemy]) {
 }
 
 
+pub fn inventory_menu(inventory: &[Object], header: &str, root: &mut Root) -> Option<usize> {
+    // how a menu with each item of the inventory as an option
+    let options = if inventory.len() == 0 {
+        vec!["Inventory is empty.".into()]
+    } else {
+        inventory.iter().map(|item| item.name.clone()).collect()
+    };
 
-// fn menu<T: AsRef<str>>(header: &str, options: &[T], width: i32, root: &mut Root) -> Option<usize> {
+    let inventory_index = menu(header, &options, INVENTORY_WIDTH, root);
 
-// }
+    // if an item was chosen, return it
+    if inventory.len() > 0 {
+        inventory_index
+    } else {
+        None
+    }
+}
+
+
+fn menu<T: AsRef<str>>(header: &str, options: &[T], width: i32, root: &mut Root) -> Option<usize> {
+  assert!(
+    options.len() <= MAX_INVENTORY,
+    format!("Cannot have a menu with more than {} options.", MAX_INVENTORY)
+  );
+
+  // calculate total height for the header (after auto-wrap) and one line per option
+  let header_height = root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header);
+  let height = options.len() as i32 + header_height;
+
+  // create an off-screen console that represents the menu's window
+  let mut window = Offscreen::new(width, height);
+
+  // print the header, with auto-wrap
+  window.set_default_foreground(tcod::colors::WHITE);
+  window.print_rect_ex(
+      0,
+      0,
+      width,
+      height,
+      BackgroundFlag::None,
+      TextAlignment::Left,
+      header,
+  );
+  
+  // print all the options
+  for (index, option_text) in options.iter().enumerate() {
+    let menu_letter = (b'a' + index as u8) as char;
+    let text = format!("({}) {}", menu_letter, option_text.as_ref());
+    window.print_ex(
+        0,
+        header_height + index as i32,
+        BackgroundFlag::None,
+        TextAlignment::Left,
+        text,
+    );
+  }
+
+  // blit the contents of "window" to the root console
+  let x = SCREEN_WIDTH / 2 - width / 2;
+  let y = SCREEN_HEIGHT / 2 - height / 2;
+  blit(&window, (0, 0), (width, height), root, (x, y), 1.0, 0.7);
+
+  // present the root console to the player and wait for a key-press
+  root.flush();
+  let key = root.wait_for_keypress(true);
+
+  // convert the ASCII code to an index; if it corresponds to an option, return it
+  if key.printable.is_alphabetic() {
+      let index = key.printable.to_ascii_lowercase() as usize - 'a' as usize;
+      if index < options.len() {
+          Some(index)
+      } else {
+          None
+      }
+  } else {
+      None
+  }
+}
