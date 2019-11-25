9d108a52adcf8d48e3db5b69daf27a2bf5b79f0b
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-11-26 00:13:54 +0100

    add new constants

--- src/constants.rs
+++ src/constants.rs
@@ -50,4 +50,6 @@ pub const LIGHTNING_DAMAGE: i32 = 40;
 pub const LIGHTNING_RANGE: i32 = 5;
 pub const CONFUSE_RANGE: i32 = 8;
 pub const CONFUSE_NUM_TURNS: i32 = 10;
+pub const FIREBALL_RADIUS: i32 = 3;
+pub const FIREBALL_DAMAGE: i32 = 12;
 
